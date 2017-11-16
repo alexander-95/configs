6d1c54bc24d86ad734e9ed3b091e5ee837b71190
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2017-11-16 17:58:38 +0000

    added more color schemes

--- install.sh
+++ install.sh
@@ -31,7 +31,10 @@ function jre
 
 function colorscheme
 {
+    default="#073642:#dc322f:#859900:#b58900:#268bd2:#ec0048:#2aa198:#94a3a5:#586e75:#cb4b16:#859900:#b58900:#268bd2:#d33682:#2aa198:#6c71c4"
     monokai="#75715e:#f92672:#a6e22e:#f4bf75:#66d9ef:#ae81ff:#2AA198:#f9f8f5:#272822:#f92672:#a6e22e:#f4bf75:#66d9ef:#ae81ff:#2AA198:#f8f8f2"
+    aci="#363636:#ff0883:#83ff08:#ff8308:#0883ff:#8308ff:#08ff83:#b6b6b6:#424242:#ff1e8e:#8eff1e:#ff8e1e:#1e8eff:#8e1eff:#1eff8e:#c2c2c2"
+    aco="#3f3f3f:#ff0883:#83ff08:#ff8308:#0883ff:#8308ff:#08ff83:#bebebe:#474747:#ff1e8e:#8eff1e:#ff8e1e:#1e8eff:#8e1eff:#1eff8e:#c4c4c4"
     gsettings set org.pantheon.terminal.settings palette $monokai
     gsettings set org.pantheon.terminal.settings foreground "#94a3a5"
     gsettings set org.pantheon.terminal.settings background "rgba(0,0,0,0.85)"
@@ -46,3 +49,5 @@ colorscheme
 #cursor-color #839496
 #background rgba(0, 0, 0, 0.85)
 #foreground #94a3a5
+
+
